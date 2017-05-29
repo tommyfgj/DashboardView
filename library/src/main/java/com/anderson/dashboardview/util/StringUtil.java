4a1fb6b961d89f64e0a435d81eb94edbdf210b9d
tommyfgj <412816322@qq.com>
2017-05-29 16:45:32 +0800

    Update StringUtil.java

--- library/src/main/java/com/anderson/dashboardview/util/StringUtil.java
+++ library/src/main/java/com/anderson/dashboardview/util/StringUtil.java
@@ -7,14 +7,14 @@ import java.math.BigDecimal;
  */
 public class StringUtil {
     /**
-     * float 转成一位小数
+     * float 转成两位位小数
      *
      * @param value
      * @return
      */
     public static String floatFormat(float value) {
         BigDecimal bd = new BigDecimal(value);
-        bd = bd.setScale(1, BigDecimal.ROUND_HALF_DOWN);
+        bd = bd.setScale(2, BigDecimal.ROUND_HALF_DOWN);
         return bd.toString();
     }
 }
